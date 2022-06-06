cc30f5e28b8785d61ff2b02d46c8aba5af4339db
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-06-06 20:33:35 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import data from '../data';
+
+describe('Testa o component <FavoritePokemons.js />', () => {
+  it('Testa se é exibida mensagem específica se não tem pokémon favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavoriteMessage = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavoriteMessage).toBeInTheDocument();
+  });
+
+  it('Testa se são exibidos os cards dos pokemon favoritos', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ data } />);
+    data.forEach((pokemon) => {
+      const pokemonName = screen.getByText(`${pokemon.name}`);
+      expect(pokemonName).toBeInTheDocument();
+    });
+  });
+});
