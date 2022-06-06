9a8ba8b7682a646b70d63ba04931396ea7914e01
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-06-06 20:45:57 -0300

    Requisito 4 feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testa o component <NotFound.js />', () => {
+  it('Testa se a página contém um heading h2 com o texto Page request not found', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen
+      .getByRole('heading', { name: /page requested not found/i, level: 2 });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Testa se a página mostra a imagem do Pikachu triste', () => {
+    renderWithRouter(<NotFound />);
+    const pikachuImageURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const pikachuImage = screen.getByRole('img', { name: /pikachu crying/i });
+
+    expect(pikachuImage.src).toBe(pikachuImageURL);
+  });
+});
